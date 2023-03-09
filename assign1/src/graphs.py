c6b01b7f262ddd4f7484ed16786c0c49bbce0a52
Joao <joaorolareis@gmail.com>
2023-03-09 11:31:42 +0000

    CSVs

--- assign1/src/graphs.py
+++ assign1/src/graphs.py
@@ -14,7 +14,7 @@ def on_close(event):
 
 independent_variable = 'Size'
 
-"""
+
 for dependent_variable in df1.columns[1:]:
     fig = plt.figure()
     plt.scatter(df1[independent_variable], df1[dependent_variable])
@@ -65,7 +65,6 @@ for dependent_variable in df4.columns[1:]:
     fig.canvas.mpl_connect('close_event', on_close)
     plt.show()
 
-"""
 # Read in the CSV file and create a data frame
 df5 = pd.read_csv('cpp_2_b.csv')
 df6 = pd.read_csv('cpp_3.csv')
@@ -96,19 +95,18 @@ for dependent_variable in df5.columns[1:]:
     plt.show()
 
 """
-measures = [{'Size': 4096, 'Block Size': 128, 'Time': '52.302', 'L1 DCM': '10202785115', 'L2 DCM': '34382345538', 'L2 DCA': '1075277551', 'L2 DCH': '-33307067987', 'DOUBLE PRECISION FLOPS': '137975824385'}
-,{'Size': 4096, 'Block Size': 256, 'Time': '56.104', 'L1 DCM': '9311210115', 'L2 DCM': '23598552262', 'L2 DCA': '580138966', 'L2 DCH': '-23018413296', 'DOUBLE PRECISION FLOPS': '137707388929'}
-,{'Size': 4096, 'Block Size': 512, 'Time': '58.173', 'L1 DCM': '8869295726', 'L2 DCM': '20806771466', 'L2 DCA': '363326939', 'L2 DCH': '-20443444527', 'DOUBLE PRECISION FLOPS': '137573171201'}
-,{'Size': 6144, 'Block Size': 128, 'Time': '177.194', 'L1 DCM': '34450150017', 'L2 DCM': '115010194696', 'L2 DCA': '3227836150', 'L2 DCH': '-111782358546', 'DOUBLE PRECISION FLOPS': '465668407297'}
-,{'Size': 6144, 'Block Size': 256, 'Time': '189.617', 'L1 DCM': '31428662381', 'L2 DCM': '80071975258', 'L2 DCA': '1787045095', 'L2 DCH': '-78284930163', 'DOUBLE PRECISION FLOPS': '464762437633'}
-,{'Size': 6144, 'Block Size': 512, 'Time': '197.138', 'L1 DCM': '29966631520', 'L2 DCM': '70173580543', 'L2 DCA': '1216808984', 'L2 DCH': '-68956771559', 'DOUBLE PRECISION FLOPS': '464309452801'}
-,{'Size': 8192, 'Block Size': 128, 'Time': '422.181', 'L1 DCM': '81566614709', 'L2 DCM': '271616070680', 'L2 DCA': '12488998376', 'L2 DCH': '-259127072304', 'DOUBLE PRECISION FLOPS': '1103806595073'}
-,{'Size': 8192, 'Block Size': 256, 'Time': '569.877', 'L1 DCM': '74446684576', 'L2 DCM': '171917055060', 'L2 DCA': '4870850041', 'L2 DCH': '-167046205019', 'DOUBLE PRECISION FLOPS': '1101659111425'}
-,{'Size': 8192, 'Block Size': 512, 'Time': '547.249', 'L1 DCM': '70939319805', 'L2 DCM': '152439345494', 'L2 DCA': '2839301971', 'L2 DCH': '-149600043523', 'DOUBLE PRECISION FLOPS': '1100585369601'}]
+measures = [ 
+{'Size': 4096, 'Block Size': 128, 'Time': '51.470', 'L1 DCM': '10204396156', 'L2 DCM': '34638931087', 'L2 DCA': '1100493932', 'L2 DCH': '-33538437155', 'DOUBLE PRECISION FLOPS': '137975824385'}
+,{'Size': 4096, 'Block Size': 256, 'Time': '56.828', 'L1 DCM': '9309893283', 'L2 DCM': '23937769856', 'L2 DCA': '605417794', 'L2 DCH': '-23332352062', 'DOUBLE PRECISION FLOPS': '137707388929'}
+,{'Size': 4096, 'Block Size': 512, 'Time': '64.558', 'L1 DCM': '8870683352', 'L2 DCM': '20226138352', 'L2 DCA': '358734097', 'L2 DCH': '-19867404255', 'DOUBLE PRECISION FLOPS': '137573171201'}
+,{'Size': 6144, 'Block Size': 128, 'Time': '177.898', 'L1 DCM': '34430620043', 'L2 DCM': '114938579223', 'L2 DCA': '3235251083', 'L2 DCH': '-111703328140', 'DOUBLE PRECISION FLOPS': '465668407297'}
+,{'Size': 6144, 'Block Size': 256, 'Time': '188.104', 'L1 DCM': '31428886659', 'L2 DCM': '79700397202', 'L2 DCA': '1766707508', 'L2 DCH': '-77933689694', 'DOUBLE PRECISION FLOPS': '464762437633'}
+,{'Size': 6144, 'Block Size': 512, 'Time': '197.525', 'L1 DCM': '29966365215', 'L2 DCM': '69121528386', 'L2 DCA': '1179905542', 'L2 DCH': '-67941622844', 'DOUBLE PRECISION FLOPS': '464309452801'}
+]
 keys = ['Size','Block Size','Time','L1 DCM', 'L2 DCM', 'L2 DCA', 'L2 DCH','DOUBLE PRECISION FLOPS']
-with open(f"cpp_3.csv", mode='w', newline='') as file:
+with open(f"cpp_3_teste.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(keys)
         for measure in measures:
             writer.writerow(list(measure.values()))
-            """
+"""
