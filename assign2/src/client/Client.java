7b2b1fb6bd78cc98ab7280fcec7c68f455f612e6
rui <mrswager173@gmail.com>
2023-05-18 12:05:10 +0100

    game is kinda functional

--- assign2/src/client/Client.java
+++ assign2/src/client/Client.java
@@ -221,7 +221,6 @@ public class Client {
 
                     if (user.ready()) {
                         out.println(user.readLine());
-                        break;
                     }
                 }
             }
